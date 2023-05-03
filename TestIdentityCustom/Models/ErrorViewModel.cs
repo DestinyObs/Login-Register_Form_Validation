1331eb217800d6069d7a69dcdc529dca72907731
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-05-03 12:10:48 +0100

    Add project files.

---
+++ TestIdentityCustom/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestIdentityCustom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
